91128bb678d24e7c4ba281fe7da0683f35ba797e
Michael Pedzimaz <mpedzi03@gmail.com>
2020-08-29 23:08:24 -0500

    Setup updates and assets directory startup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-lg-6 profilecard mb-2">
+          <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+            <b-row no-gutters>
+              <b-col md="6">
+                <b-card-img :src="profilePicture" alt="Image" class="rounded-0">
+                </b-card-img>
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Michael Pedzimaz">
+                  <b-card-text>
+                    Full stack developer with Java, Coldfusion, and JavaScript
+                    expertise.
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+        </div>
+        <div class="col-md-12 col-lg-6 profilecard">
+          <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+            <b-row no-gutters>
+              <b-col md="6">
+                <b-card-img :src="profilePicture" alt="Image" class="rounded-0">
+                </b-card-img>
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Michael Pedzimaz">
+                  <b-card-text>
+                    Full stack developer with Java, Coldfusion, and JavaScript
+                    expertise.
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+        </div>
+      </div> <!--/row-->
+    </div> <!--/container-->
+    <div class="container mt-5">
+      <div class="row bg-primary">
+        <div class="col">
+          <p class="bg-secondary">Test paragraph</p>
+        </div>
+        <div class="col">
+          <p class="bg-danger">Test paragraph</p>
+        </div>
+        <div class="col">
+          <p class="bg-info">Test paragraph</p>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import profilePicture from '@/assets/img/ProfilePic.jpg'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+    }
+  },
+  computed: {
+    profilePicture () {
+      return profilePicture
+    }
   }
 }
 </script>
+<style scoped>
+.profilecard {
+  opacity: 0.7;
+}
+</style>
